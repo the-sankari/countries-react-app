6c76ed59700aae52cb3e23ad644b4de7806ba02a
Kajol sutra dhar <kajol.sd777@gmail.com>
2024-10-22 22:22:45 +0300

    Modified Navebar for mobile view

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -9,60 +9,61 @@ const Header = () => {
 
   return (
     <Navbar
+      collapseOnSelect
+      expand="md"
+      bg="dark"
+      variant="dark"
+      className="justify-content-end align-items-center"
       style={{ marginBottom: "1rem", backgroundColor: "#d0e5ff" }}
-      // bg="light"
-      // variant="light"
     >
-      <Container className="justify-content-end">
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="me-auto">
-            <LinkContainer to="/">
-              <Nav.Link className="fw-bold">Home</Nav.Link>
-            </LinkContainer>
-            <LinkContainer to="/countries">
-              <Nav.Link className="fw-bold">Countries</Nav.Link>
-            </LinkContainer>
-            <LinkContainer to="/favourites">
-              <Nav.Link className="fw-bold">Favourites</Nav.Link>
-            </LinkContainer>
-            {/* Conditionally render Register and Login links */}
-            {!user ? (
-              <>
-                <LinkContainer to="/register">
-                  <Nav.Link className="fw-bold">Register</Nav.Link>
-                </LinkContainer>
-                <LinkContainer to="/login">
-                  <Nav.Link className="fw-bold">Login</Nav.Link>
-                </LinkContainer>
-              </>
-            ) : (
-              <>
-                <Button
-                  onClick={logout}
-                  className="fw-bold"
-                  style={{
-                    color: "black",
-                    backgroundColor: "#d0e5ff",
-                    border: "none",
-                  }}
-                >
-                  Logout
-                </Button>
-                <div
+      <Navbar.Toggle aria-controls="basic-navbar-nav" />
+      <Navbar.Collapse id="basic-navbar-nav">
+        <Nav className="me-auto">
+          <LinkContainer to="/">
+            <Nav.Link className="fw-bold">Home</Nav.Link>
+          </LinkContainer>
+          <LinkContainer to="/countries">
+            <Nav.Link className="fw-bold">Countries</Nav.Link>
+          </LinkContainer>
+          <LinkContainer to="/favourites">
+            <Nav.Link className="fw-bold">Favourites</Nav.Link>
+          </LinkContainer>
+          {/* Conditionally render Register and Login links */}
+          {!user ? (
+            <>
+              <LinkContainer to="/register">
+                <Nav.Link className="fw-bold">Register</Nav.Link>
+              </LinkContainer>
+              <LinkContainer to="/login">
+                <Nav.Link className="fw-bold">Login</Nav.Link>
+              </LinkContainer>
+            </>
+          ) : (
+            <>
+              <Button
+                onClick={logout}
+                className="fw-bold"
+                variant="outline-secondary"
+                size="sm"
+              >
+                Logout
+              </Button>
+              <LinkContainer to="/user">
+                <Nav.Link
+                  className="fw-bold text-success"
                   style={{
                     marginLeft: "50rem",
                     marginTop: "0.5rem",
                   }}
-                  className="text-success"
                 >
+                  {" "}
                   Welcome, {user.email.split("@")[0]}
-                </div>
-              </>
-            )}
-          </Nav>
-        </Navbar.Collapse>
-      </Container>
+                </Nav.Link>
+              </LinkContainer>
+            </>
+          )}
+        </Nav>
+      </Navbar.Collapse>
     </Navbar>
   );
 };
--- src/pages/Layout.jsx
+++ src/pages/Layout.jsx
@@ -3,71 +3,16 @@ import Row from "react-bootstrap/Row";
 import { Outlet } from "react-router-dom";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
-import { auth, logout } from "../auth/firebase";
-import { useAuthState } from "react-firebase-hooks/auth";
-import { Button, Nav, Navbar } from "react-bootstrap";
-import { LinkContainer } from "react-router-bootstrap";
+
+
 
 const Layout = () => {
-  const [user] = useAuthState(auth);
 
   return (
     <Container fluid>
       <Row>
         {/* Header Section */}
-        <Navbar collapseOnSelect expand="md" bg="dark" variant="dark"className="justify-content-end align-items-center"
-         style={{ marginBottom: "1rem", backgroundColor: "#d0e5ff" }}
-        >
-          <Navbar.Toggle aria-controls="basic-navbar-nav" />
-          <Navbar.Collapse id="basic-navbar-nav">
-            <Nav className="me-auto">
-              <LinkContainer to="/">
-                <Nav.Link className="fw-bold">Home</Nav.Link>
-              </LinkContainer>
-              <LinkContainer to="/countries">
-                <Nav.Link className="fw-bold">Countries</Nav.Link>
-              </LinkContainer>
-              <LinkContainer to="/favourites">
-                <Nav.Link className="fw-bold">Favourites</Nav.Link>
-              </LinkContainer>
-              {/* Conditionally render Register and Login links */}
-              {!user ? (
-                <>
-                  <LinkContainer to="/register">
-                    <Nav.Link className="fw-bold">Register</Nav.Link>
-                  </LinkContainer>
-                  <LinkContainer to="/login">
-                    <Nav.Link className="fw-bold">Login</Nav.Link>
-                  </LinkContainer>
-                </>
-              ) : (
-                <>
-                  <Button
-                    onClick={logout}
-                    className="fw-bold"
-                    variant="outline-secondary"
-                    size="sm"
-                  >
-                    Logout
-                  </Button>
-                  <LinkContainer to="/user">
-                    <Nav.Link
-                      className="fw-bold text-success"
-                      style={{
-                        marginLeft: "50rem",
-                        marginTop: "0.5rem",
-                      }}
-                    >
-                      {" "}
-                      Welcome, {user.email.split("@")[0]}
-                    </Nav.Link>
-                  </LinkContainer>
-                  
-                </>
-              )}
-            </Nav>
-          </Navbar.Collapse>
-        </Navbar>
+        <Header />
       </Row>
       <Row style={{ flex: "1", minHeight: "calc(100vh-69px)" }}>
         {" "}
