20bb6d5c1fbc5895a981d3df4b6aafec92ea377e
Kajol sutra dhar <kajol.sd777@gmail.com>
2024-10-23 02:53:52 +0300

    Adjusted the Layout

--- src/pages/Layout.jsx
+++ src/pages/Layout.jsx
@@ -4,17 +4,21 @@ import { Outlet } from "react-router-dom";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
 
-
-
 const Layout = () => {
-
   return (
     <Container fluid>
       <Row>
         {/* Header Section */}
         <Header />
       </Row>
-      <Row style={{ flex: "1", minHeight: "calc(100vh-69px)", marginTop: "56px", marginBottom:"130px" }}>
+      <Row
+        style={{
+          flex: "1",
+          minHeight: "calc(100vh-69px)",
+          marginTop: "56px",
+          marginBottom: "130px",
+        }}
+      >
         {" "}
         <Outlet />
       </Row>
