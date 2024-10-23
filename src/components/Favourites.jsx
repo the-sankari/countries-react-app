8e16b346e75383e9b4d4be97baec97b15fe07090
Kajol sutra dhar <kajol.sd777@gmail.com>
2024-10-23 05:45:23 +0300

    Debug: Bugs for favourites

--- src/components/Favourites.jsx
+++ src/components/Favourites.jsx
@@ -1,49 +1,28 @@
-// Favourites to be written.
 import { useEffect, useState } from "react";
 import { Button, Col, Container, Form, Row, Spinner } from "react-bootstrap";
 import { useDispatch, useSelector } from "react-redux";
-import { initializeCountries } from "../services/countriesServices";
+import { initializeCountries } from "../services/countriesServices"; // Import the service correctly
 import {
   clearFavourites,
   getFavouritesFromSource,
 } from "../store/favouriteSlice";
 import CountriesCard from "./CountriesCard";
 
-// Favourites to be written
+// Favourites component
 const Favourites = () => {
   const dispatch = useDispatch();
-  let countriesList = useSelector((state) => state.countries.countries);
+  const countriesList = useSelector((state) => state.countries.countries);
+  const [search, setSearch] = useState("");
   const countriesLoading = useSelector((state) => state.countries.isLoading);
   const favouritesList = useSelector((state) => state.favourites.favourites);
   const favouritesLoading = useSelector((state) => state.favourites.isLoading);
-  const [search, setSearch] = useState("");
 
-  console.log("favouritesList: ", favouritesList);
-  console.log("countriesList inside favourites: ", countriesList);
-
-  // Filter favourite list countries
-  const filteredCountries =
-    Array.isArray(favouritesList) && favouritesList.length > 0
-      ? countriesList.filter((country) =>
-          favouritesList.includes(country.name.common)
-        )
-      : [];
-
-  // if (Array.isArray(favouritesList) && favouritesList.length > 0) {
-  //   countriesList = countriesList.filter((country) =>
-  //     favouritesList.includes(country.name.common)
-  //   );
-  // } else {
-  //   countriesList = [];
-  // }
-
-  // Fetch the data on component mount
+  // Fetch countries and favourites on component mount
   useEffect(() => {
     dispatch(initializeCountries());
     dispatch(getFavouritesFromSource());
   }, [dispatch]);
 
-  // Display loading spinner while data is loading
   if (countriesLoading || favouritesLoading) {
     return (
       <Col className="text-center m-5">
@@ -59,9 +38,16 @@ const Favourites = () => {
     );
   }
 
+  // Filter countries based on favourites
+  const filteredCountries =
+    Array.isArray(favouritesList) && favouritesList.length > 0
+      ? countriesList.filter((country) =>
+          favouritesList.includes(country.name.common)
+        )
+      : [];
+
   return (
     <Container fluid>
-      {/* Search Input bar */}
       <Row>
         <Col className="mt-5 d-flex justify-content-center">
           <Form>
@@ -76,15 +62,11 @@ const Favourites = () => {
           </Form>
         </Col>
       </Row>
-      {/* Clear favourite button */}
-      <Row xs={2} md={3} lg={4} className="g-3 mt-3">
-        <Col className="d-flex justify-content.center">
-          <Button onClick={() => dispatch(clearFavourites())}>
-            Clear Favourites
-          </Button>
-        </Col>
+      <Row xs={2} md={3} lg={4} className="g-3">
+        <Button onClick={() => dispatch(clearFavourites())}>
+          Clear Favourites
+        </Button>
       </Row>
-      {/* Display countries */}
       <Row xs={2} md={3} lg={4} className="g-3">
         {filteredCountries
           .filter((country) =>
